e0371d7eeecb2a54d95cdbe4aa8d2b0cd640e67a
nola360 <adaramolajr@gmail.com>
2023-10-14 23:28:41 -0400

    updated dashboard features

---
+++ src/components/EmissionTracker.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+function EmissionsTracker() {
+  const [emissions, setEmissions] = useState([]);
+  const [newEmission, setNewEmission] = useState('');
+
+  const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+
+  // Fetch emissions from the API
+  useEffect(() => {
+    fetch('https://api.v2.emissions-api.org/api/v2/countries.json', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }, []);
+
+  // Function to handle adding a new emission
+  const addEmission = () => {
+    if (newEmission.trim() === '') return;
+
+    // Here, you can implement the logic to add a new emission to the API if needed.
+
+    // Clear the input field
+    setNewEmission('');
+  };
+
+  return (
+    <div>
+      <h2>Emissions Tracker</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter an emission"
+          value={newEmission}
+          onChange={(e) => setNewEmission(e.target.value)}
+        />
+        <button onClick={addEmission}>Add Emission</button>
+      </div>
+      <ul>
+        {emissions.map((emission) => (
+          <li key={emission.id}>{emission.sourceName}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default EmissionsTracker;
