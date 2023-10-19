94b20632e1aa459ce8c1ccc267f4525081444ff4
[Kenney Kong] <kenneykong@gmail.com>
2023-10-18 22:06:23 -0400

    updated Login.css and added logos

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,25 +3,27 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Brand = styled.div`
-  color: white;
-  font-size: 1.5rem;
+  width: 100px;
+  height: 100px;
 `;
 
 const NavBar = styled.nav`
-  background-color: #282c34;
+  background-color: #00000000;
   display: flex;
   justify-content: space-between;
-  padding: 10px;
+
   position: sticky;
-  top: 0;
+  top: 20px;
 `;
 
 const NavLink = styled(Link)`
-  color: white;
+  color: #00FF85;
   text-decoration: none;
-  margin: 0 10px;
-  font-size: 1rem;
-
+  margin: 0 10px 0 ;
+  font-size: 1.15rem;
+  font-weight: 600;
+ line-height: 40px;
+ 
   &:hover {
     text-decoration: underline;
   }
@@ -30,7 +32,7 @@ const NavLink = styled(Link)`
 function Navbar() {
   return (
   <NavBar>
-    <Brand>Scoop</Brand>
+      <Brand><img src="data/icon.svg" alt="Scoop" width="140px" /></Brand>
     <div>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/dashboard">Heat Map</NavLink>
