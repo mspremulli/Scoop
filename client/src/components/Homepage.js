3c9094ff6400879890c80d1909137d66ae0015ec
Christian Elijah Alcantara <chrsalctra@icloud.com>
2023-10-20 08:39:49 -0400

    Resolve merge conflict: Home page JS file

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import Vanta from './Vanta'
-
-import ChatBot from './ChatBot';
 import './Homepage.css';
-import styled from 'styled-components';
-
-var Container = styled.div`
-`
+import ChatBot from './ChatBot';
 
 const HomePage = () => (
-  
-  <Container>
-    { <header style={{ minHeight: '100vh', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-    <div className="image-container" >
+  <div class="container">
+    <div class="left">
+    <div className="image-container">
       <div className="welcome">
         <h2>Welcome to Scoop!</h2>
         <p>Explore our features and start carpooling today!</p>
-        
       </div>
-     
     </div>
-    <ChatBot />
-    <div style={{position: 'fixed', width: '5000px', zIndex: -1}}><Vanta ></Vanta></div>
-    </header> }
-  </Container>
-  
-  
+    </div>
+    <div class="right">
+      <ChatBot />
+    </div>
+  </div>
 );
 
-
 export default HomePage;
+
